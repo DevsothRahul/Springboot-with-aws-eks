086e292c8f5ea6dc8f8521a31409767470c74230
RahulD-at-592788499043 <rahul.devsoth@techwave.net>
2024-09-12 19:48:35 +0530

    First Commit

---
+++ src/main/java/com/techwave/demo/Controller/WelcomeController.java
@@ -0,0 +1,42 @@
+package com.techwave.demo.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.techwave.demo.pojo.User;
+import com.techwave.demo.repo.UserRepository;
+
+@RestController
+public class WelcomeController {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@GetMapping("/")
+	public String home() {
+		return "<h1>HOME<h1>";
+	}
+
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "<h1>Hello, Welcome to SprinBoot <h1>";
+	}
+
+	@GetMapping("/all")
+	public List<User> getAllUser() {
+		return (List<User>) userRepository.findAll();
+	}
+
+	@PostMapping("/adduser")
+	public void createUser(@RequestBody User user) {
+		System.out.println("hello");
+		userRepository.save(user);
+
+	}
+
+}
---
+++ src/main/java/com/techwave/demo/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.techwave.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EntityScan(basePackages = "com.techwave.demo.pojo")
+@EnableJpaRepositories(basePackages = "com.techwave.demo.repo")
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+
+		System.out.println("\n \n Welcome to Sprinboot Application........");
+	}
+
+}
